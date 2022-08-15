52092bc1a127080e0e9a5ae56b8e04b2f4cfc577
Erika <adob.e@hotmail.com>
2022-08-15 17:06:40 +0200

    feat: animate first brush in #home

--- js/script.js
+++ js/script.js
@@ -14,4 +14,37 @@ setTimeout(()=> {
 }, 2000)
 
 
-// 
+// BRUSH TEXT
+
+// Variables :
+/// First Brush : 
+const FirstBrushLeft = document.querySelector('#brush-right');
+const blackCube = document.querySelector('#black-cube');
+const dicesGoBack = document.querySelectorAll('.goBack')
+const distanceFromTheTopFirst = FirstBrushLeft.getBoundingClientRect().top;
+/// Second Brush:
+const SecondBrush = document.querySelector('#brush-visibility');
+const distanceFromTheTopSecond = SecondBrush.getBoundingClientRect().top;
+
+/// Third Brush
+const thirdBrushRight = document.querySelector('#brush-left');
+const distanceFromTheTopThird = thirdBrushRight.getBoundingClientRect().top;
+
+console.log(scrollY);
+
+
+
+window.addEventListener('scroll', () => {  
+    let scrollY = window.scrollY;
+
+    if (scrollY >= distanceFromTheTopFirst -200 ){
+        setTimeout(() => {
+            dicesGoBack.forEach( dice => {
+                dice.style.zIndex = '-6';
+            })
+        }, 530);
+        blackCube.classList.add('cube-left');
+    } 
+    
+    
+})
