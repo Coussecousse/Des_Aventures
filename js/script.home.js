b6054d9fb6b869648a40bf28bbe78370ed0719db
Erika <adob.e@hotmail.com>
2022-08-23 16:35:26 +0200

    fix : mobile version, dice rollin #script.home.js

--- js/script.home.js
+++ js/script.home.js
@@ -4,14 +4,16 @@ const whiteDice = document.querySelector("#white-dice");
 const greenDice = document.querySelector("#green-dice");
 
 ///Fonctions :
-greenDice.classList.add("Gdice__move-in");
-whiteDice.classList.add("Wdice__move-in");
-setTimeout(() => {
-  whiteDice.classList.add("Wdice__wiggle");
-}, 1500);
-setTimeout(() => {
-  greenDice.classList.add("Wdice__wiggle");
-}, 2000);
+window.addEventListener('load', e => {
+  greenDice.classList.add("Gdice__move-in");
+  whiteDice.classList.add("Wdice__move-in");
+  setTimeout(() => {
+    whiteDice.classList.add("Wdice__wiggle");
+  }, 1500);
+  setTimeout(() => {
+    greenDice.classList.add("Wdice__wiggle");
+  }, 2000);
+})
 
 // BRUSH TEXT
 
